c88672964602661448b86202308035c45e6f6317
Maya Brito <may20brito@gmail.com>
2024-08-16 18:35:34 +0000

    Adding version implementations to the tests.

--- LuLu/LuLuTests/CortexLogsTests.m
+++ LuLu/LuLuTests/CortexLogsTests.m
@@ -14,24 +14,100 @@
 
 @implementation CortexLogsTests
 
-- (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+// Método auxiliar para simular o mapeamento do CSV para JSON
+- (NSDictionary *)mapCSVLineToJSON:(NSString *)csvLine {
+    NSArray *fields = [csvLine componentsSeparatedByString:@"\t"];
+
+    // Exemplo de mapeamento.
+    NSString *key = fields[0];
+    NSString *uuid = [[NSUUID UUID] UUIDString];
+    NSString *path = fields[1];
+    NSString *name = fields[2];
+    NSString *endpointAddr = fields[4];
+    NSString *endpointPort = fields[6];
+
+    return @{
+        key: @[
+            @{
+                @"key": key,
+                @"uuid": uuid,
+                @"path": path,
+                @"name": name,
+                @"endpointAddr": endpointAddr,
+                @"endpointPort": endpointPort,
+                @"isEndpointAddrRegex": @"0",
+                @"type": @"1",
+                @"scope": @"0",
+                @"action": @"1",
+                @"csInfo": @{
+                    @"signatureIdentifier": @""
+                }
+            }
+        ]
+    };
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+// Teste para validar o mapeamento da chave 'key'
+- (void)testKeyMapping {
+    NSString *csvLine = @"/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player\t/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player\tQuickTime Player\t64465\t142.250.218.74\toptimizationguide-pa.googleapis.com\t443\t1\tTRUE";
+
+    NSDictionary *json = [self mapCSVLineToJSON:csvLine];
+    NSString *expectedKey = @"/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player";
+    
+    XCTAssertTrue([json objectForKey:expectedKey] != nil, @"Key should be mapped correctly.");
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+// Teste para validar o mapeamento de UUID
+- (void)testUUIDMapping {
+    NSString *csvLine = @"/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player\t/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player\tQuickTime Player\t64465\t142.250.218.74\toptimizationguide-pa.googleapis.com\t443\t1\tTRUE";
+
+    NSDictionary *json = [self mapCSVLineToJSON:csvLine];
+    NSString *key = @"/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player";
+    NSString *uuid = json[key][0][@"uuid"];
+    
+    NSUUID *testUUID = [[NSUUID alloc] initWithUUIDString:uuid];
+    XCTAssertNotNil(testUUID, @"UUID should be valid and correctly formatted.");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+// Teste para validar o mapeamento de endereço IP e porta
+- (void)testEndpointMapping {
+    NSString *csvLine = @"/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player\t/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player\tQuickTime Player\t64465\t142.250.218.74\toptimizationguide-pa.googleapis.com\t443\t1\tTRUE";
+
+    NSDictionary *json = [self mapCSVLineToJSON:csvLine];
+    NSString *key = @"/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player";
+
+    NSString *expectedAddr = @"142.250.218.74";
+    NSString *expectedPort = @"443";
+    
+    XCTAssertEqualObjects(json[key][0][@"endpointAddr"], expectedAddr, @"Endpoint address should be mapped correctly.");
+    XCTAssertEqualObjects(json[key][0][@"endpointPort"], expectedPort, @"Endpoint port should be mapped correctly.");
+}
+
+// Teste para validar a presença do campo 'csInfo'
+- (void)testCsInfoMapping {
+    NSString *csvLine = @"/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player\t/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player\tQuickTime Player\t64465\t142.250.218.74\toptimizationguide-pa.googleapis.com\t443\t1\tTRUE";
+
+    NSDictionary *json = [self mapCSVLineToJSON:csvLine];
+    NSString *key = @"/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player";
+    NSDictionary *csInfo = json[key][0][@"csInfo"];
+
+    XCTAssertNotNil(csInfo, @"csInfo should be present in the JSON.");
+    XCTAssertEqualObjects(csInfo[@"signatureIdentifier"], @"", @"signatureIdentifier should be correctly mapped.");
+}
+
+// Teste para validar que todos os atributos obrigatórios estão presentes
+- (void)testMandatoryAttributes {
+    NSString *csvLine = @"/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player\t/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player\tQuickTime Player\t64465\t142.250.218.74\toptimizationguide-pa.googleapis.com\t443\t1\tTRUE";
+
+    NSDictionary *json = [self mapCSVLineToJSON:csvLine];
+    NSString *key = @"/System/Applications/QuickTime Player.app/Contents/MacOS/QuickTime Player";
+
+    NSDictionary *entry = json[key][0];
+    NSArray *mandatoryKeys = @[@"key", @"uuid", @"path", @"name", @"endpointAddr", @"endpointPort", @"isEndpointAddrRegex", @"type", @"scope", @"action", @"csInfo"];
+    
+    for (NSString *mandKey in mandatoryKeys) {
+        XCTAssertNotNil(entry[mandKey], @"Mandatory attribute %@ should be present in the JSON.", mandKey);
+    }
 }
 
 @end
